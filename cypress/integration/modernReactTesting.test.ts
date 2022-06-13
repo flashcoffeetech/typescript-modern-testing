392a823c5fd1b111132d26e58e8083ee63ec66bd
Robby Widyahartono <robby.widyahartono@flash-coffee.com>
2022-06-13 15:56:17 +0700

    fix eslint

--- cypress/integration/modernReactTesting.test.ts
+++ cypress/integration/modernReactTesting.test.ts
@@ -1,39 +1,39 @@
-import { errorHandlers } from '../../src/mocks/errorHandlers';
+import { errorHandlers } from "../../src/mocks/errorHandlers";
 
-it('When clicking on the fetch posts button it should list out the posts', () => {
-  cy.visit('/');
-  cy.window().should('have.property', 'appReady', true);
-  cy.findByRole('heading', { name: /Modern React Testing/i, level: 1 }).should(
-    'exist',
+it("When clicking on the fetch posts button it should list out the posts", () => {
+  cy.visit("/");
+  cy.window().should("have.property", "appReady", true);
+  cy.findByRole("heading", { name: /Modern React Testing/i, level: 1 }).should(
+    "exist"
   );
-  cy.findByRole('button', { name: 'Fetch Posts' }).click();
-  cy.findByLabelText('loading').should('exist');
-  cy.findAllByRole('heading', { level: 3 }).should('exist');
+  cy.findByRole("button", { name: "Fetch Posts" }).click();
+  cy.findByLabelText("loading").should("exist");
+  cy.findAllByRole("heading", { level: 3 }).should("exist");
 });
 
-it('When clicking on the clear posts button it should clear the posts.', () => {
-  cy.findByRole('heading', { name: /Modern React Testing/i, level: 1 }).should(
-    'exist',
+it("When clicking on the clear posts button it should clear the posts.", () => {
+  cy.findByRole("heading", { name: /Modern React Testing/i, level: 1 }).should(
+    "exist"
   );
-  cy.findByRole('button', { name: 'Clear Posts' }).click();
-  cy.findAllByRole('heading', { level: 3 }).should('not.exist');
+  cy.findByRole("button", { name: "Clear Posts" }).click();
+  cy.findAllByRole("heading", { level: 3 }).should("not.exist");
 });
 
-it('Display error message when fetch posts data failed', async () => {
-  cy.visit('/');
-  cy.window().should('have.property', 'appReady', true);
+it("Display error message when fetch posts data failed", async () => {
+  cy.visit("/");
+  cy.window().should("have.property", "appReady", true);
   cy.window().then((window: any) => {
     const { worker } = window.msw;
     worker.resetHandlers();
     worker.use(...errorHandlers);
   });
-  cy.findByRole('heading', {
+  cy.findByRole("heading", {
     name: /Modern React Testing/i,
     level: 1,
-  }).should('exist');
-  cy.findByRole('button', { name: 'Fetch Posts' }).click();
-  cy.findByLabelText('loading').should('exist');
-  cy.findByLabelText('Internal server error').should('exist');
+  }).should("exist");
+  cy.findByRole("button", { name: "Fetch Posts" }).click();
+  cy.findByLabelText("loading").should("exist");
+  cy.findByLabelText("Internal server error").should("exist");
 });
 
 // describe("When clicking on the fetch posts button it should list out the posts", () => {
