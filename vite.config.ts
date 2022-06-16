4d6ea2821fef2d94169f5fc5c776f54ac00507ea
Robby Widyahartono <robby.widyahartono@flash-coffee.com>
2022-06-16 10:53:52 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setup.ts'],
-    reporters: 'verbose',
+    environment: "jsdom",
+    setupFiles: ["./src/setup.ts"],
+    reporters: "verbose",
     coverage: {
-      reporter: ['text', 'json', 'html'],
+      reporter: ["text", "json", "html"],
     },
   },
 });
